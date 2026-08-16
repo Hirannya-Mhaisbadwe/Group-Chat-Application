a0ec5b6c995ec2e2003b7c6a055515abb538ea51
Hirannya Mhaisbadwe <hirannyamhaisbadwe@gmail.com>
2026-08-16 21:26:36 +0530

    fix: set CORSMiddleware allow_credentials to False to allow origin wildcard

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,7 @@ app = FastAPI(title="NexChat Server")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
